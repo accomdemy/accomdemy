83e7b51f5b3a59d8fe99fdd620c896053987d43d
andrewintw <andrew.lin@browan.com>
2021-09-08 21:58:47 +0800

    add for 0908 - jupyter-lab

---
+++ slides/0908 - jupyter-lab/arduino/DHT11_tester/DHT11_tester.ino
@@ -0,0 +1,26 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#include "DHT.h"
+#define DHTPIN 2     // what digital pin we're connected to
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println(h);
+}
